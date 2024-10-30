f3d3a4bd58dc2ef01683a2301b44398ab7344275
Ricky Valencia <3D.RickyV@gmail.com>
2024-10-29 22:34:01 -0700

    clean up

--- app.py
+++ app.py
@@ -10,27 +10,8 @@ from games.framed_game_api import FramedGameApi
 from games.games import Games
 from games.rules.game_rule import FramedGameRule
 
-
 config = dotenv_values(".env")
 
-def get_game(channel):
-    conn = sqlite3.connect("leaderboard.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM game WHERE channel_id = ?", (channel.id,))
-    game = cursor.fetchone()
-    conn.close()
-    return game
-
-def parse_score(user, game, content):
-    conn = sqlite3.connect("leaderboard.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM user WHERE discord_user_id = ?", (user.id,))
-    user = cursor.fetchone()
-    cursor.execute("SELECT * FROM game WHERE game_name = ?", (game,))
-    game = cursor.fetchone()
-    cursor.execute("INSERT INTO score (user_id, game_id, score, date_submitted) VALUES (?, ?, ?, ?)", (user[0], game[0], content, "2020-08-06"))
-    conn.commit()
-    conn.close()
 
 if __name__ == '__main__':
     sqlite = SqliteDatabase(config["DB_NAME"])
